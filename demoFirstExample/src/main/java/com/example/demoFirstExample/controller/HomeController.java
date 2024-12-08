214e273d16d45808ba11e68ac4d48c91b92db827
Mukeshvarma777 <gmukeshvarma423@gmail.com>
2024-12-08 18:48:06 +0530

    second commit

---
+++ demoFirstExample/src/main/java/com/example/demoFirstExample/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.example.demoFirstExample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.demoFirstExample.entity.Alien;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("home")
+	public ModelAndView home(Alien alien)
+	{
+		ModelAndView mv=new ModelAndView();
+		mv.addObject("obj",alien);
+		mv.setViewName("home");
+		return mv;
+	}
+	
+
+}
---
+++ demoFirstExample/src/test/java/com/example/demoFirstExample/DemoFirstExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoFirstExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoFirstExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
